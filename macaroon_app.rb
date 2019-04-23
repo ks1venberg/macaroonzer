aba48871ace6c766ec771bb48b6bd49bf54d025e
ks1venberg <venberg.dev@gmail.com>
2019-04-23 17:53:07 +0300

    created layout with navigation, bootstrap added

--- macaroon_app.rb
+++ macaroon_app.rb
@@ -2,4 +2,19 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
+# require 'bootstrap'
+
+#=============================
+
+class App < Sinatra::Base
+end
+
+get '/' do
+	erb :index			
+end
+
+get '/info' do
+	erb :info
+end
+
 
