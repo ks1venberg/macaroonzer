8e6774573a9107655155476cf1378a2bc36c9dff
ks1venberg <venberg.dev@gmail.com>
2019-04-26 11:12:48 +0300

    short view for empty cart

--- macaroon_app.rb
+++ macaroon_app.rb
@@ -35,6 +35,10 @@ post '/cart' do
 	@orders_input = params[:orders_input]
 	@items = parse_orders_input @orders_input
 
+	if @items.length ==0
+		return erb :empty_cart
+	end
+
 	@items.each do |item|
 		item[0] = Product.find(item[0])
 	end
