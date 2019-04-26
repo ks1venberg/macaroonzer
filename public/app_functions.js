1ae75eba644730f851596a71c1694786e0a7bebb
ks1venberg <venberg.dev@gmail.com>
2019-04-26 10:53:38 +0300

    added javascript functions to put orders in cart, cart view

---
+++ public/app_functions.js
@@ -0,0 +1,57 @@
+function add_to_cart(id)
+{
+  var key = 'product_' + id;
+  
+  var x = window.localStorage.getItem(key);
+    x = x*1 + 1;
+    window.localStorage.setItem(key, x);
+  update_cart();
+  update_orders_button();
+}
+
+function update_cart()
+{
+  var orders = cart_get_orders();
+  $('#orders_input').val(orders);
+
+}
+
+function update_orders_button()
+{
+  var text = 'Cart (' + cart_items() + ')';
+  $('#orders_button').val(text);
+}
+
+function cart_get_orders()
+{
+    var orders = '';
+
+  for(var i=0; i < window.localStorage.length; i++)
+  {
+    var key = window.localStorage.key(i);
+    var value = window.localStorage.getItem(key);
+
+    if(key.indexOf('product_') == 0)
+    {
+      orders = orders + key + '=' + value + ',';
+    }
+  }
+  return orders;
+}
+
+function cart_items()
+{
+    var cnt = 0;
+
+  for(var i=0; i < window.localStorage.length; i++)
+  {
+    var key = window.localStorage.key(i);
+    var value = window.localStorage.getItem(key);
+
+    if(key.indexOf('product_') == 0)
+    {
+      cnt = cnt + value*1;
+    }
+  }
+  return cnt;
+}
